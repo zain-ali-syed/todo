d6e9b81509de55155094d2c32f5537d95e512e56
messagetobeats <chelnov63@hotmail.com>
2019-04-07 18:33:11 +0100

    entered some comments to db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+//please enter your username(yourDBUsername) and password(yourDBPassword) below
+//also replace the path after the '@' with your url to your DB
+
 //connect to mongodb
-mongoose.connect('mongodb://root:zain123@ds237748.mlab.com:37748/todos_db', { useNewUrlParser: true }, () => console.log("connected"))
+mongoose.connect('mongodb://yourDBUsername:yourDBPassword@ds237748.mlab.com:37748/todos_db', { useNewUrlParser: true }, () => console.log("connected"))
 mongoose.Promise = global.Promise;
